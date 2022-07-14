1ee20d38e29c58003f0b60d6b12a2d674804bcb2
@nafi <berhanunafyad977@gmail.com>
2022-07-14 12:45:13 -0700

    challenge

---
+++ #week4b challenge.py
@@ -0,0 +1,5 @@
+#week4b challenge
+base = float(input("the length of the base"))
+height = float(input("the length of the height"))
+Area = (base * height)/2
+print("the area of this triangel is", Area)
